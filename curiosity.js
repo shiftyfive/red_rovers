06939b4d069e2b2ffa30ec4abc7efb46ebfa6eb8
Joshua Warren <joshuawarren000@gmail.com>
2017-03-17 09:10:05 -0700

    made some changes

--- curiosity.js
+++ curiosity.js
@@ -1,145 +1,81 @@
-// moment.calendarFormat = function(myMoment, now) {
-//     var diff = myMoment.diff(now, 'days', true);
-//     var nextMonth = now.clone().add(1, 'month');
-//
-//     var retVal = diff < -6 ? 'sameElse' :
-//         diff < -1 ? 'YYYY-MM-DD' :
-//         diff < 0 ? 'YYYY-MM-DD' :
-//         diff < 1 ? 'YYYY-MM-DD' :
-//         diff < 2 ? 'YYYY-MM-DD' :
-//         diff < 7 ? 'YYYY-MM-DD' :
-//         // introduce thisMonth and nextMonth
-//         (myMoment.month() === now.month() && myMoment.year() === now.year()) ? 'thisMonth' :
-//         (nextMonth.month() === myMoment.month() && nextMonth.year() === myMoment.year()) ? 'nextMonth' : 'sameElse';
-//     return retVal;
-// };
 
-var manifestObj;
-
-var lastSevenDays = [];
-
-(function() {
-    let dateHolder;
-    for (let i = 0; i < 7; i++) {
-        dateHolder = moment().subtract(i, 'days').format("YYYY-MM-DD");
-        lastSevenDays.push(dateHolder)
-    }
-
-}());
-
-function shuffle(array) { //downloaded this off stack overflow
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-function imgDimensionsCheck(img){
-  $("#curiosity-img-check").html(`<img hidden id="check-img" src=${img}>`);
-  return document.getElementById("check-img").naturalWidth;
-}
-
+var roverObj;
 var imgArr = [];
 
-function renderImgs() {
+// function imgDimensionsCheck(img) {
+//   var $container = $(".img-check")
+//   var $myImg = $(`<img src='${img}'/>`)
+//   $container.prepend($myImg)
+//   setTimeout(function() {
+//     return $myImg[0].width
+//
+//   }, 200)
+// }
 
+function render(arr) {
+  if (arr.length === 0) {
+    alert("oops it looks like there were no photos of note that day sorry :(")
+  } else {
+    for (let value of arr) {
+      console.log(value)
+      $(".carousel.carousel-slider").append(`<a class="carousel-item" href=${value}><img src=${value}></a>`)
+    }
+  }
 }
 
 function getPhotosDay(date, camera) {
-    $.ajax({
-        method: "GET",
-        url: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=${camera}&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi`,
-        success: function(results) {
-            console.log("Done", results);
-            var roverObj = results;
-            shuffle(roverObj["photos"])
-            roverObj["photos"].forEach(function(element) {
-                if (imgArr.length < 25) {
-                  var imgWidth = imgDimensionsCheck(element["img_src"])
-                  if(imgWidth > 300){
-                    imgArr.push(element["img_src"]);
-                  }
-                } else {
-                  return imgArr;
-                }
-            })
-        },
-
-        error: function(error) {
-            console.log("Error: ", error)
-        }
-    })
-};
-
-
-function getPhotosWeek(dates, camera) {
-  var accArr = [];
-  for (var i = 0; i < dates.length; i++) {
-    $.ajax({
-    method: "GET",
-    url: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${dates[i]}&camera=${camera}&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi`,
-    success: function(results) {
-      console.log("done: ", results)
-      var roverObj = results
-      shuffle(roverObj["photos"])
-      accArr = accArr.concat(roverObj["photos"])
-    },
-
-    error: function(error) {
-      console.log("Error: ", error)
-      }
-    })
-  }
-};
-
-function getMissionManifest() {
   $.ajax({
     method: "GET",
-    url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-01-05&camera=mast&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi",
-    success: function(result) {
-      console.log("done: ", result)
-      manifestObj = result["photos"][0]["rover"];
-        let $roverManifest = (`
-          <li>Launch Date: ${manifestObj["launch_date"]}</li>
-          <li>
-          <li>Landing Date: ${"landing_date"}`)
-      }
-    },
+    url: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=${camera}&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi`,
+    success: function(results) {
+      console.log(results)
+      roverObj = results.photos
 
+      for (var i = 0; i < 4; i++) {
+        if ( i < 4) {
+          console.log(roverObj[i]["img_src"])
+          $(".row.img-row1").append(`<div class="col s12 m3 responsive-img"><img style="max-height:220px;max-width:257px;padding:5px" src='${roverObj[i]["img_src"]}'></div>`)
+        } else {
+          return roverObj;
+        }
+      }
+      return roverObj;
+    },
     error: function(error) {
-      console.log("error: ", result)
+      console.error(error);
+      alert("Sorry it doesn't look like any photos were taken with this camera on that day")
     }
   })
-  $("#curiosity-manifest-collection").append($roverManifest);
+};
+
+function notify() {
+  alert( "clicked" );
 }
+$( ".card-action a" ).on( "click", getPhotosDay("2016-01-02", "mast" ));
 
-getMissionManifest();
+// $("mast-button").click(getPhotosDay("2016-1-1", "mast"))
 
-// function getPhotosWeek(lastWeekArr) {
-//     let roverPicArr =
+
+
 //
-//         $.ajax({
-//             method: "GET",
-//             url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-1-3&camera=mast&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi",
-//             success: function(results) {
-//                 console.log("Done", results);
-//                 pictures = results
-//             },
+// function getMissionManifest() {
+//   $.ajax({
+//     method: "GET",
+//     url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-01-05&camera=mast&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi",
+//     success: function(result) {
+//       console.log("done: ", result)
+//       manifestObj = result["photos"][0]["rover"];
+//         let $roverManifest = (`
+//           <li>Launch Date: ${manifestObj["launch_date"]}</li>
+//           <li>
+//           <li>Landing Date: ${"landing_date"}`)
+//       }
+//     },
 //
-//             error: function(error) {
-//                 console.log("Error: ", error)
-//             }
-//         });
+//     error: function(error) {
+//       console.log("error: ", result)
+//     }
+//   })
+//   $("#curiosity-manifest-collection").append($roverManifest);
+// }
+//
