4fcbe04ff3a06086adaddfbb3f2615784a7eeaff
Joshua Warren <joshuawarren000@gmail.com>
2017-03-17 10:22:24 -0700

    Minimum Presentable Product.

--- curiosity.js
+++ curiosity.js
@@ -1,81 +1,108 @@
+$(document).ready(function() {
+  var roverObj;
+  var imgArr = [];
 
-var roverObj;
-var imgArr = [];
+  // function imgDimensionsCheck(img) {
+  //   var $container = $(".img-check")
+  //   var $myImg = $(`<img src='${img}'/>`)
+  //   $container.prepend($myImg)
+  //   setTimeout(function() {
+  //     return $myImg[0].width
+  //
+  //   }, 200)
+  // }
 
-// function imgDimensionsCheck(img) {
-//   var $container = $(".img-check")
-//   var $myImg = $(`<img src='${img}'/>`)
-//   $container.prepend($myImg)
-//   setTimeout(function() {
-//     return $myImg[0].width
-//
-//   }, 200)
-// }
-
-function render(arr) {
-  if (arr.length === 0) {
-    alert("oops it looks like there were no photos of note that day sorry :(")
-  } else {
-    for (let value of arr) {
-      console.log(value)
-      $(".carousel.carousel-slider").append(`<a class="carousel-item" href=${value}><img src=${value}></a>`)
+  function render(arr) {
+    if (arr.length === 0) {
+      alert("oops it looks like there were no photos of note that day sorry :(")
+    } else {
+      for (let value of arr) {
+        console.log(value)
+        $(".carousel.carousel-slider").append(`<a class="carousel-item" href=${value}><img src=${value}></a>`)
+      }
     }
   }
-}
 
-function getPhotosDay(date, camera) {
-  $.ajax({
-    method: "GET",
-    url: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=${camera}&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi`,
-    success: function(results) {
-      console.log(results)
-      roverObj = results.photos
 
-      for (var i = 0; i < 4; i++) {
-        if ( i < 4) {
-          console.log(roverObj[i]["img_src"])
-          $(".row.img-row1").append(`<div class="col s12 m3 responsive-img"><img style="max-height:220px;max-width:257px;padding:5px" src='${roverObj[i]["img_src"]}'></div>`)
-        } else {
-          return roverObj;
+  let photoCall = function(date, camera) {
+    console.log('here man');
+    $.ajax({
+      method: "GET",
+      url: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=${camera}&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi`,
+      success: function(results) {
+        console.log(results)
+        roverObj = results.photos
+
+        for (var i = 0; i < 4; i++) {
+          if (i < 4) {
+            $(".rover-pics").append(`<div class="col s12 m3"><img style="max-height:220px;max-width:257px;padding:10px;" src=${roverObj[i]["img_src"]}></div>`)
+          } else {
+            return roverObj;
+          }
         }
+      },
+      error: function(error) {
+        console.error(error);
+        alert("Sorry it doesn't look like any photos were taken with this camera on that day")
       }
-      return roverObj;
-    },
-    error: function(error) {
-      console.error(error);
-      alert("Sorry it doesn't look like any photos were taken with this camera on that day")
-    }
-  })
-};
+    })
+  };
 
-function notify() {
-  alert( "clicked" );
-}
-$( ".card-action a" ).on( "click", getPhotosDay("2016-01-02", "mast" ));
+  $("#mast").on('click', function (e) {
+    e.preventDefault()
+    photoCall('2016-01-02', 'mast')
+  });
 
-// $("mast-button").click(getPhotosDay("2016-1-1", "mast"))
+  $("#chemcam").on('click', function (e) {
+    e.preventDefault()
+    photoCall('2016-01-02', 'chemcam')
+  });
+
+  $("#fhaz").on('click', function (e) {
+    e.preventDefault()
+    photoCall('2016-01-02', 'fhaz')
+  });
+
+  $("#rhaz").on('click', function (e) {
+    e.preventDefault()
+    photoCall('2016-01-02', 'rhaz')
+  });
+
+  $("#navcam").on('click', function (e) {
+    e.preventDefault()
+    photoCall('2016-01-02', 'navcam')
+  });
+// photoCall("2016-01-02", "mast"
+  // $("mast-button").click(getPhotosDay("2016-1-1", "mast"))
 
 
 
-//
-// function getMissionManifest() {
-//   $.ajax({
-//     method: "GET",
-//     url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-01-05&camera=mast&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi",
-//     success: function(result) {
-//       console.log("done: ", result)
-//       manifestObj = result["photos"][0]["rover"];
-//         let $roverManifest = (`
-//           <li>Launch Date: ${manifestObj["launch_date"]}</li>
-//           <li>
-//           <li>Landing Date: ${"landing_date"}`)
-//       }
-//     },
-//
-//     error: function(error) {
-//       console.log("error: ", result)
-//     }
-//   })
-//   $("#curiosity-manifest-collection").append($roverManifest);
-// }
-//
+  //
+  // function getMissionManifest() {
+  //   $.ajax({
+  //     method: "GET",
+  //     url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-01-05&camera=mast&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi",
+  //     success: function(result) {
+  //       console.log("done: ", result)
+  //       manifestObj = result["photos"][0]["rover"];
+  //         let $roverManifest = (`
+  //           <li>Launch Date: ${manifestObj["launch_date"]}</li>
+  //           <li>
+  //           <li>Landing Date: ${"landing_date"}`)
+  //       }
+  //     },
+  //
+  //     error: function(error) {
+  //       console.log("error: ", result)
+  //     }
+  //   })
+  //   $("#curiosity-manifest-collection").append($roverManifest);
+  // }
+  //
+
+
+
+
+
+
+});
