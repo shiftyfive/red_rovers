c7d4c9c4deb159714fe4d4539018feb052663942
Joshua <joshua@JW-dev.local>
2017-03-31 13:04:11 -0700

    continued refactoring

--- curiosityRefactor.js
+++ curiosityRefactor.js
@@ -1,49 +1,50 @@
 
-
-
 let picArray = [];
+let lastPhotoTakenDate;
+let proxy = "https://galvanize-cors-proxy.herokuapp.com/"
 
-let lastSevenDays = [];
-
-function getDates(startDate) {
-  let dayToPush;
-  for (var i = 0; i < 7; i++) {
-    dayToPush = moment().subtract(i, "days").format("YYYY-MM-DD");
-    lastSevenDays.push(dayToPush);
-  }
-};
-
-function collectImgs(arr) {
-  for (var i = 0; i < 30; i++) {
-    picArray.push(arr[i]);
-  }
-};
-
-
-function ajaxRequest(date, camera) {
+function getLastPhotoDate () {
   $.ajax({
     method: "GET",
-    url: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=${camera}&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi`,
+    url: `${proxy}https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-1-5&camera=mast&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi`,
     success: function(results) {
-      console.log(results);
-      roverObj = results.photos;
-
-      collectImgs(roverObj);
+      lastPhotoTakenDate = results.photos[0].rover.max_date
 
     },
     error: function(error) {
-      console.error(error);
+      console.log("error", error)
     }
   })
-};
+}
 
+
+function appendCarousel (date, camera){
+let appendCarousel = new Promise (resolve, reject) {
+  $.ajax({
+    method: "GET",
+    url: `${proxy}https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=${camera}&api_key=xcbRk9wwyuts3TVWoW6Yh0SigQ0JcRUegzSAwaZi`,
+    success: function(results) {
+      console.log(results);
+      return roverObj = results.photos;
+
+    },
+    error: function(error) {
+      alert("oops it looks like something went wrong ☹️");
+    }
+    })
+  }
+}
+
+  appendCarousel.then(function(roverObj){
+
+  })
 
 $('.picture-cards').on('click', 'a', callAjax)
 
 function callAjax(event) {
   event.preventDefault();
   let selectedCamera = $(event.target).attr("id");
-  photoCall(lastSevenDays, selectedCamera)
+  appendCarousel(lastPhotoTakendate, selectedCamera)
 };
 
 function photoCall(date, camera) {
@@ -57,3 +58,10 @@ function photoCall(date, camera) {
     }
   }
 };
+
+
+function renderImgs(roverObj) {
+  roverObj.forEach(function (imgSrc){
+    <a class="carousel-item" href="#"><img src=`imgSrc`></a>
+  })
+}
